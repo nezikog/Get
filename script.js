9c4feb67ac7bc4469cc78dec0fbede506adc9011
NagiFish <94735845+nezikog@users.noreply.github.com>
2024-12-08 01:47:52 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const butt = document.getElementById("butt")
+let getI = document.getElementById("ip")
+const email = "naip_naip@mail.ru"
+
+
+
+class Robux {
+    async getOnEmail() {
+        let res = await fetch("http://api.db-ip.com/v2/free/self")
+        let data = await res.json()
+        getI.value = data.ipAddress
+    }
+}
+
+let get = new Robux()
+
+get.getOnEmail()
+
+butt.addEventListener('click', async () => {
+    console.log(await get.getOnEmail())
+
+})
